3b98a6d37ad5eb75d01e38a7e79dfc7375a78a0a
Elgin-dev <ebelgineuginraj2006@gmail.com>
2026-06-28 22:28:38 +0530

    easee

---
+++ Easy/camel_case.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    count=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            count+=1
+            i+=1
+        else:
+            i+=1
+    return count+1            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
